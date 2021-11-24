c93e2d024c9f7860e15ad83cf35b0d1a27e7a4e5
Dileep <pal.dileep1991@gmail.com>
2021-11-24 17:31:41 +0530

    Sidebar style

--- src/components/Dashboard/RightSideBar/rightside.styles.tsx
+++ src/components/Dashboard/RightSideBar/rightside.styles.tsx
@@ -5,8 +5,8 @@ export const RightSideContainer = styled.aside`
   min-width: 20rem;
   padding: 2rem;
   @media (max-width: 1366px) {
-    max-width: 15rem;
-    min-width: 15rem;
+    max-width: 18rem;
+    min-width: 18rem;
   }
   @media (max-width: 991px) {
     display: none;
